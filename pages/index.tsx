0fb9ad13457558a093aaec097f02198c94bce9f5
DenysDevelopment <dinis_05@ukr.net>
2021-12-04 08:59:11 +0200

    index change design

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,7 +41,7 @@ const Home: NextPage = () => {
 	return (
 		<Layout title='Portfolio'>
 			<h1 className={style.title}>Портфолио</h1>
-			<div className={`${style.wrapper} ${projects.length ? style.df : ''}`}>
+			<div className={`${style.wrapper}`}>
 				{projects.length
 					? projects.map((project, idx) => (
 							<PortfolioItem {...project} key={`${project.name}_${idx}`} />
